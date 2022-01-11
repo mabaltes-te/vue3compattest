572bbc291d8aa57be48c81f150afd686fb10eda3
Mathew Baltes <mabaltes@thousandeyes.com>
2022-01-11 11:55:47 -0800

    wip including tooltip and modal

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,18 +5,70 @@
     <TeLink :on-click="onClickAlert">This is a onclick link label</TeLink>
     <br/>
     <TeLink href="https://www.thousandeyes.com" title="This is a normal link title">This is a normal link label</TeLink>
+
+
+    <TeButton
+        ref="button0"
+        :on-click="() => isOpenBasic = true"
+        class="mb-3">
+      Open Basic Modal
+    </TeButton>
+    <TeModal
+        :is-open="isOpenBasic"
+        :on-close="() => isOpenBasic = false"
+        title="Some Title">
+      <template v-slot:body>
+        <div>
+           This is a test
+        </div>
+      </template>
+    </TeModal>
+
+    <div class="tooltip-story">
+      <div class="mb-3">
+        <h4> Hover example </h4>
+        <TeTooltip
+            offset="10"
+            popover-class="tetip"
+            container=".tooltip-story"
+            trigger="hover"
+            placement="right">
+          <span>Hover over me</span>
+          <template v-slot:popover>
+            TeTooltip example
+          </template>
+        </TeTooltip>
+      </div>
+      <div class="mb-3">
+        <h4> Click example </h4>
+        <TeTooltip
+            offset="10"
+            popover-class="tetip"
+            container=".tooltip-story"
+            placement="left">
+          <span>Click on me</span>
+          <template v-slot:popover>
+            TeTooltip example
+          </template>
+        </TeTooltip>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { TeLink } from '@te/ui-components';
-
-console.log(TeLink);
+import { TeButton, TeLink, TeModal, TeTooltip } from '@te/ui-components';
 
 export default {
   components: {
-    TeLink
+    TeLink,
+    TeModal,
+    TeTooltip,
+    TeButton
   },
+  data: () => ({
+    isOpenBasic: false,
+  }),
   methods: {
     onClickAlert: () => alert('onClick link clicked')
   },
